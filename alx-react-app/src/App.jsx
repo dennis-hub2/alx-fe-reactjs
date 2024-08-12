630b16ab8435ef4feb1e4c510d8f18b641146222
dennis-hub2 <dennisankora17@gmail.com>
2024-08-12 00:07:38 +0000

    Reactjs

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,9 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import React from 'react';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
