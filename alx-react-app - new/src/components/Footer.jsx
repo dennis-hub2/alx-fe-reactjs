a8c242fda07a58d84a45f4bf7ac5fd515a738a55
dennis-hub2 <dennisankora17@gmail.com>
2024-08-18 22:02:42 +0000

    styling with react

---
+++ alx-react-app - new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{padding: '10px', backgroundColor: 'blueviolet', fontStyle: 'italic'} }>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app - new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+// MainContent.jsx
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{fontStyle:'italic' }}> 
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
