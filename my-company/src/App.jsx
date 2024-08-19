dbabe07c63599e45f88319b809198ee23742728c
dennis-hub2 <dennisankora17@gmail.com>
2024-08-19 04:33:24 +0000

    my company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './components/Contact';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: 'white', justifyContent: 'center', padding: '10px', }}>
+    <nav>
       <Link to="/" style={{ margin: '10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '10px' }}>About</Link>
       <Link to="/services" style={{ margin: '10px' }}>Services</Link>
--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
+    <nav> style={{ backgroundColor: 'white', justifyContent: 'center', padding: '10px', }}
       <Link to="/" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Services</Link>
