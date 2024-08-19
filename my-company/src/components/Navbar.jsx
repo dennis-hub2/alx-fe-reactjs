def35f9968a340110e7984ef0fd8d695d2052070
dennis-hub2 <dennisankora17@gmail.com>
2024-08-19 04:32:04 +0000

    my company

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Home</Link>
+      <Link to="/about" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>About</Link>
+      <Link to="/services" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
