2dc91f6909eaf7697914c70f7f3882d9ebc2f478
dennis-hub2 <dennisankora17@gmail.com>
2024-08-19 04:30:51 +0000

    my company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './components/Contact';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#f0f0f0' }}>
+    <nav style={{ backgroundColor: 'white', justifyContent: 'center', padding: '10px', }}>
       <Link to="/" style={{ margin: '10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '10px' }}>About</Link>
       <Link to="/services" style={{ margin: '10px' }}>Services</Link>
