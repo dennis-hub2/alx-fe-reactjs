9633c5f4a8905ad977b2462cb07adcaf508ecea4
dennis-hub2 <dennisankora17@gmail.com>
2024-08-19 00:58:00 +0000

    react

---
+++ my-company/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '10px', background: '#f0f0f0' }}>
+      <Link to="/" style={{ margin: '10px' }}>Home</Link>
+      <Link to="/about" style={{ margin: '10px' }}>About</Link>
+      <Link to="/services" style={{ margin: '10px' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '10px' }}>Contact</Link>
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
