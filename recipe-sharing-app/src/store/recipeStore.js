2ee314f8ce2479510988e424aed0849b832815ce
dennis-hub2 <dennisankora17@gmail.com>
2024-09-01 23:50:34 +0000

    recipe-sharing-app

---
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -0,0 +1,18 @@
+// src/stores/recipeStore.js
+import create from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
