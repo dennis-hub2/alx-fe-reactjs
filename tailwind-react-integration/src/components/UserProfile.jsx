2273ae3a78c7d8744387bb781517f44069ea74e9
dennis-hub2 <dennisankora17@gmail.com>
2024-09-08 23:51:26 +0000

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,12 +2,12 @@ function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
-        src="https://via.placeholder.com/150"
+        src= 'src/assets/react.svg '
         alt="User"
         className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
-        Dennis
+        Dennis Ankora
       </h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600">
         Engineer in the making and love to explore new technologies.
