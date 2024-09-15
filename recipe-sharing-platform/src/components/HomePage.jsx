3a9abccdb4ff60884259360392442791a37ee1ec
dennis-hub2 <dennisankora17@gmail.com>
2024-09-15 21:40:33 +0000

    Building the Recipe Detail Page

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import data from "../data.json"; // Import the mock data
 
 const HomePage = () => {
