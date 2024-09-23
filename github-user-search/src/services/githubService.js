c31d5095a866d2dc76880b57a158d2a7ff977536
dennis-hub2 <dennisankora17@gmail.com>
2024-09-23 00:01:22 +0000

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,8 +4,20 @@ import axios from 'axios';
 const BASE_URL = 'https://api.github.com/search/users';
 
 // Function to search GitHub users with advanced filters
-export const searchGitHubUsers = async (query) => {
+export const searchGitHubUsers = async (username, location, minRepos) => {
   try {
+    // Construct the query string
+    let query = `${username}`;
+
+    // Add optional filters
+    if (location) {
+      query += `+location:${location}`;
+    }
+    if (minRepos) {
+      query += `+repos:>${minRepos}`;
+    }
+
+    // Make the API request with the constructed query
     const response = await axios.get(`${BASE_URL}?q=${query}`);
     return response.data;
   } catch (error) {
